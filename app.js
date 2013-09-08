48353055326a3480f47c5dd21091f15c26b6b110
Myles Borins <myles.borins@gmail.com>
2013-09-08 02:05:05 -0700

    This is getting hawt

--- app.js
+++ app.js
@@ -6,11 +6,30 @@ var path = require('path');
 
 var sourceDir = process.argv[2];
 
-FS.list(sourceDir)
+FS.exists(sourceDir + '/ogg/')
+.then(function (exists) {
+  'use strict';
+  if (!exists) {
+    return FS.makeDirectory(sourceDir + '/ogg/').then(FS.exists(sourceDir + '/wmv/'));
+  }
+  else {
+    return FS.exists(sourceDir + '/wmv/');
+  }
+})
+.then(function (exists) {
+  'use strict';
+  if (!exists) {
+    return FS.makeDirectory(sourceDir + '/wmv/').then(FS.exists(FS.list(sourceDir + '/mov/')));
+  }
+  else {
+    return FS.list(sourceDir + '/mov/');
+  }
+})
+.then(FS.list(sourceDir + '/mov/'))
 .then(function (list) {
   'use strict';
   var count = 0;
-  
+
   var convert = function (path) {
     console.log(path);
 
@@ -20,14 +39,15 @@ FS.list(sourceDir)
     }, function (err) {
       console.log('Error: ' + err);
     });
-    
+
   };
-  
+
   var movFile = function (fileName) {
     return path.extname(fileName) === '.mov';
   };
-  
+
   list = _.filter(list, movFile);
-  
+
   _.each(list, convert);
-});
+})
+.done();
