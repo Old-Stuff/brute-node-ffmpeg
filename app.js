f8a159d9446d7919e581700459a0739ee1d43d32
Myles Borins <myles.borins@gmail.com>
2013-09-08 00:44:30 -0700

    promises :D

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var ffmpeg = require('ffmpeg');
+var _ = require('underscore');
+var q = require('q');
+var FS = require('q-io/fs');
 
+FS.list(__dirname)
+.then(function (list) {
+  'use strict';
+  console.log(list);
+});
 
 try {
   var original = new ffmpeg(__dirname + '/samples/prores422_29.97.mov');
